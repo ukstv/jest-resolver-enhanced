70e91fe9cf30fc2c8f3450e57af0a4e41db7900a
Sergey Ukustov <sergey@ukstv.me>
2022-10-28 21:21:29 +0300

    chore: Try without JS extension

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const fs = require('fs');
-const resolve = require('enhanced-resolve');
+const fs = require('fs')
+const resolve = require('enhanced-resolve')
 
 const enhanced = resolve.ResolverFactory.createResolver({
     fileSystem: new resolve.CachedInputFileSystem(fs, 4000),
     extensions: ['.js', '.json'],
     useSyncFileSystemCalls: true,
     conditionNames: ['node', 'require'],
-});
+})
 
-module.exports = (request, options) => {
+function resolution(request, options) {
     try {
-        return enhanced.resolveSync({}, options.basedir, request, {});
+        return enhanced.resolveSync({}, options.basedir, request, {})
     } catch {
+        if (request.match(/(\w+).js$/)) {
+            const noJS = request.replace(/\.js$/, '')
+            return resolution(noJS, options)
+        }
         // Call Jest default resolver
-        return options.defaultResolver(request, options);
+        return options.defaultResolver(request, options)
     }
-};
+}
+
+module.exports = resolution
